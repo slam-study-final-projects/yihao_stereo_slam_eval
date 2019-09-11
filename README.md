909c5565b85d12fd4a4f300ba9552a35ef5ea00d
t <yihao.wei@deptrum.com>
2019-09-11 10:05:30 +0800

    Init

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# yihao_stereo_slam_eval
-A runnable repo.
+#
+
+A repo for the final project of SLAM study.  
+
+## Project Structure
+
+1. **SlamWork**: Body class of all the features.
+2. **DepthGenerator**: Generate the depth from stereo images.  
+3. **IcpTranslator**: Generate the R and T between 2 frames.
+
+
+
+## TODO 
+
+1. Add backend features.
+2. Debug the wrong translation according to the reference.
+
+
+
+## Result
+
+![result](/home/t/slam_study/PA8_code/code/result.png)
+
+
+
+Input argument: The exact **kitti** data directory of images: **00,01,02,...**
+
+Output now are the infos printed at the console. 
+
+**R_world**: The R matrix from the present to the first.
+
+**t_world**: The t vector from the present to the first.
+
---
+++ slam_work.h
@@ -0,0 +1,35 @@
+#include "sophus/se3.hpp"
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <pangolin/pangolin.h>
+#include <string>
+#include <unistd.h>
+#include <vector>
+
+#include "depth_generator.h"
+#include "icp_translator.h"
+#include "orb_matcher.h"
+
+using namespace std;
+
+class SlamWork {
+public:
+  SlamWork();
+  ~SlamWork() = default;
+  void SetParams(double fx, double fy, double cx, double cy, double d);
+  void ComputePose();
+
+  string input_path_;
+  int pose_number_;
+
+  DepthGenerator depth_generator_;
+  OrbMatcher orb_matcher_;
+  IcpTranslator icp_translator_;
+
+  Eigen::Matrix3d R_world_;
+  Eigen::Vector3d t_world_;
+};
